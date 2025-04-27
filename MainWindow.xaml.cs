0adeb95d225bb7d43f39be6a02837580dd41a893
Ryu <196807182+Ryu-prog@users.noreply.github.com>
2025-04-28 07:38:05 +0900

    新規作成

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop.Word;
+using System.ComponentModel;
+using System.IO;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace WordImageReplace
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.TemplatePath)){
+                this.tbWordFilePath.Text = Properties.Settings.Default.TemplatePath;
+            }
+
+            if (!String.IsNullOrEmpty(Properties.Settings.Default.FrontFilePath))
+            {
+                this.tbFrontFilePath.Text = Properties.Settings.Default.FrontFilePath;
+            }
+
+            //if (!String.IsNullOrEmpty(Properties.Settings.Default.BackFilePath))
+            //{
+            //    this.tbBackFilePath.Text = Properties.Settings.Default.BackFilePath;
+            //}
+
+            //if (!String.IsNullOrEmpty(Properties.Settings.Default.Password))
+            //{
+            //    this.pbWordFile.Password = Properties.Settings.Default.Password;
+            //}
+
+            Closing += FormMain_FormClosing;
+        }
+
+        /// <summary>
+        /// アプリ終了時のプロパティ保存
+        /// </summary>
+        private void FormMain_FormClosing(object sender, CancelEventArgs e)
+        {
+            Properties.Settings.Default.TemplatePath = this.tbWordFilePath.Text;
+            Properties.Settings.Default.FrontFilePath = this.tbFrontFilePath.Text;
+            Properties.Settings.Default.BackFilePath = this.tbBackFilePath.Text;
+            //Properties.Settings.Default.Password = this.pbWordFile.Password;
+            Properties.Settings.Default.Save();
+
+        }
+
+
+        private void Replace_CommandButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool isChangeFront = Convert.ToBoolean(this.cbFrontSide.IsChecked);
+            bool isChangeBack = Convert.ToBoolean(this.cbBackSide.IsChecked);
+
+            string templatePath = this.tbWordFilePath.Text;
+            string frontFilePath = this.tbFrontFilePath.Text;
+            string backFilePath = this.tbBackFilePath.Text;
+            //string protectPass = this.pbWordFile.Password;
+
+            //入力欄チェック
+            if (File.Exists(templatePath) == false) {
+                System.Windows.MessageBox.Show(templatePath + "が見つかりませんでした。\n対象のファイルが存在するか確認してください。"
+                    , "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (File.Exists(frontFilePath) == false) {
+                System.Windows.MessageBox.Show(frontFilePath + "が見つかりませんでした。\n対象のファイルが存在するか確認してください。"
+                    , "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (isChangeBack && File.Exists(backFilePath) == false)
+            {
+                System.Windows.MessageBox.Show(backFilePath + "が見つかりませんでした。\n対象のファイルが存在するか確認してください。"
+                    , "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+
+
+            WordController wd = new WordController();
+            //try
+            //{
+                wd.OpenDocument(templatePath);
+
+                //wd.UnprotectPassword(protectPass);
+
+                if (isChangeFront)
+                {
+                    wd.HeaderPictureChange(WdHeaderFooterIndex.wdHeaderFooterPrimary, frontFilePath);
+                }
+
+                //if (isChangeBack)
+                //{
+                //    wd.HeaderPictureChange(WdHeaderFooterIndex.wdHeaderFooterEvenPages, backFilePath);
+                //}
+
+                //wd.ProtectPassword(protectPass);
+
+                //保存先選択ダイアログを表示
+                OpenFileDialog opd = new OpenFileDialog();
+
+                opd.Filter = "Wordファイル(*.dotx)|*.dotx";
+
+                opd.FileName = templatePath;
+
+                if ((bool)opd.ShowDialog())
+                {
+                    wd.SaveDoc(opd.FileName);
+                }
+
+
+                //保存完了メッセージ
+                System.Windows.MessageBox.Show("Wordファイルの背景画像を差し替えました。"
+                        , "正常終了", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            //}
+            //catch(Exception ex)
+            //{
+            //    System.Windows.MessageBox.Show("エラーが発生しました" + Environment.NewLine + ex.ToString(), "エラー");
+            //}
+
+            
+
+        }
+
+        private string FilePathSelect(string FilePath, string ExtensionLabel, string Extension)
+        {
+
+
+            return null;
+        }
+
+        private void WordFileSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+
+            opd.Filter = "Wordファイル(*.docx)|*.docx";
+
+            if ((bool)opd.ShowDialog())
+            {
+                this.tbWordFilePath.Text = opd.FileName;
+            }
+        }
+
+        private void FrontSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+
+            opd.Filter = "画像ファイル|*.png;*.jp*g";
+
+            if ((bool)opd.ShowDialog())
+            {
+                this.tbFrontFilePath.Text = opd.FileName;
+            }
+        }
+
+        //private void BackSelectButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    OpenFileDialog opd = new OpenFileDialog();
+
+        //    opd.Filter = "画像ファイル|*.png;*.jp*g";
+
+        //    if ((bool)opd.ShowDialog())
+        //    {
+        //        this.tbBackFilePath.Text = opd.FileName;
+        //    }
+        //}
+    }
+}
---
+++ WordController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Office.Interop;
+using Microsoft.Office.Interop.Word;
+
+namespace WordImageReplace
+{
+        public class WordController
+        {
+
+            private Microsoft.Office.Interop.Word.Application wordApplication = null;
+            private Microsoft.Office.Interop.Word.Documents wordDocuments = null;
+            private Microsoft.Office.Interop.Word.Document wordDocument = null;
+
+
+            public void OpenDocument(string filePath)
+            {
+                Application wordApp = new Application();
+                this.wordDocuments = wordApp.Documents;
+
+                object fPathObj = filePath;
+
+                // Word 文書を開く
+                this.wordDocument = wordApp.Documents.Open(ref fPathObj);
+
+                // Word アプリケーションを画面に表示する
+                wordApp.Visible = true;
+            }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+            public void UnprotectPassword(string password = "")
+            {
+
+                if (this.wordDocument.ProtectionType != WdProtectionType.wdNoProtection)
+                {
+
+                    this.wordDocument.Unprotect(password);
+                }
+            }
+
+            public void ProtectPassword(string password = "")
+            {
+
+                if (password != "")
+                {
+                    this.wordDocument.Protect(WdProtectionType.wdAllowOnlyReading, false, password);
+                }
+                else
+                {
+                    this.wordDocument.Protect(WdProtectionType.wdAllowOnlyReading, false);
+                }
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="headerIndex"></param>
+            /// <param name="imagePath"></param>
+            public void HeaderPictureChange(WdHeaderFooterIndex headerIndex, string imagePath)
+            {
+
+            ////裏面画像が存在する場合、奇数ページと偶数ページのヘッダーを分けるをON
+            //if (headerIndex == WdHeaderFooterIndex.wdHeaderFooterEvenPages)
+            //{
+
+            //    foreach (Section section in this.wordDocument.Sections)
+            //    {
+            //        section.PageSetup.OddAndEvenPagesHeaderFooter = 1;
+            //        //this.wordDocument.Sections[1].PageSetup.OddAndEvenPagesHeaderFooter = (int)WdHeaderFooterIndex.wdHeaderFooterEvenPages;
+
+
+            //    }
+            //}
+                PageSetup pageSetup = this.wordDocument.PageSetup;
+
+                double Left = 0;
+                double Top = 0;
+                double Width = pageSetup.PageWidth;
+                double Height = pageSetup.PageHeight;
+
+                foreach (Section sec in this.wordDocument.Sections)
+                {
+
+                    HeaderFooter header = sec.Headers[headerIndex];
+
+                    header.Range.Delete();
+                    header.Shapes.AddPicture(@imagePath, false, true, Left, Top, Width, Height);
+                    header.Range.ShapeRange.WrapFormat.Type = WdWrapType.wdWrapBehind;
+
+                    ShapeRange shapeRange = sec.Headers[headerIndex].Range.ShapeRange;
+
+                    shapeRange.LockAspectRatio = Microsoft.Office.Core.MsoTriState.msoTrue;
+
+                    shapeRange.RelativeVerticalPosition = WdRelativeVerticalPosition.wdRelativeVerticalPositionPage;
+
+                    shapeRange.RelativeHorizontalPosition = WdRelativeHorizontalPosition.wdRelativeHorizontalPositionPage;
+
+                    shapeRange.Left = 0;
+
+                }
+
+            }
+
+            public void SaveDoc(string saveFilePath)
+            {
+                this.wordDocument.SaveAs(FileName: saveFilePath);
+            }
+
+        }
+}
